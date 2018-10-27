6b7cf895ab87c54c93f6e5d860ba274b06807f77
davcache <dav14029@byui.edu>
2018-10-27 09:02:24 -0600

    10/27/2018 Final Submit

--- GlazerCalculation/MainPage.xaml.cs
+++ GlazerCalculation/MainPage.xaml.cs
@@ -5,6 +5,7 @@ using System.Linq;
 using System.Runtime.InteropServices.WindowsRuntime;
 using Windows.Foundation;
 using Windows.Foundation.Collections;
+using Windows.UI.ViewManagement;
 using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
 using Windows.UI.Xaml.Controls.Primitives;
@@ -25,6 +26,50 @@ namespace GlazerCalculation
         public MainPage()
         {
             this.InitializeComponent();
+
+            ApplicationView.PreferredLaunchViewSize = new Size(800, 1200);
+            ApplicationView.PreferredLaunchWindowingMode = ApplicationViewWindowingMode.PreferredLaunchViewSize;
         }
+
+        private void sldQuantity_ValueChanged(object sender, RangeBaseValueChangedEventArgs e)
+        {
+            var sldrQuantity = sender as Slider;
+
+            var quantity = sldrQuantity.Value;
+            txbUpdateQuantity.Text = quantity.ToString();
+        }
+        private void btnCaluclate_Click(object sender, RoutedEventArgs e)
+        {
+            double width, height, woodLength, glassArea;
+            string widthString, heightString;
+            widthString = txtWidth.Text;
+            width = double.Parse(widthString);
+            heightString = txtHeight.Text;
+            height = double.Parse(heightString);
+
+            woodLength = 2 * (width + height) * 3.25;
+            glassArea = 2 * (width * height);
+
+            txtWoodLength.Text = woodLength.ToString() + " feet";
+            txtGlass.Text = glassArea.ToString() + " square metres";
+        }
+
+        //class GlazerCalc
+        //{
+        //    double width, height, woodLength, glassArea;
+        //    string widthString, heightString;
+        //    Console.Write("Enter Width: ");
+        //    widthString = Console.ReadLine();
+        //    width = double.Parse(widthString);
+        //    Console.Write("Enter Height: ");
+        //    heightString = Console.ReadLine();
+        //    height = double.Parse(heightString);
+        //    woodLength = 2 * (width + height) * 3.25;
+        //    glassArea = 2 * (width * height);
+        //    Console.WriteLine("The length of the wood is " +
+        //    woodLength + " feet");
+        //    Console.WriteLine("The area of the glass is " +
+        //    glassArea + " square metres");
+        //}
     }
 }
